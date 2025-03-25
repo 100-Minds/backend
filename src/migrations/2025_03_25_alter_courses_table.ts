176c2f4c3ba39023e9a9cce15693abcc7c5aa733
Dave <uchennadavid2404@gmail.com>
2025-03-25 18:39:46 +0100

    Add description and courseImage fields to course and course chapters tables

---
+++ src/migrations/2025_03_25_alter_courses_table.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.alterTable('course', (table) => {
+		table.string('courseImage').notNullable().defaultTo('');
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.alterTable('course', (table) => {
+		table.dropColumn('courseImage');
+	});
+}
