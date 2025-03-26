76039cca7698b71526b1f96fed80509c896f949f
Dave <uchennadavid2404@gmail.com>
2025-03-26 03:26:07 +0100

    Disable secure cookie setting for all environments

--- src/common/utils/helper.ts
+++ src/common/utils/helper.ts
@@ -149,7 +149,8 @@ const setCookie = (
 	} else {
 		res.cookie(name, value, {
 			httpOnly: true,
-			secure: ENVIRONMENT.APP.ENV === 'production',
+			// secure: ENVIRONMENT.APP.ENV === 'production',
+			secure: false,
 			path: '/',
 			sameSite: ENVIRONMENT.APP.ENV === 'production' ? 'none' : 'lax',
 			partitioned: ENVIRONMENT.APP.ENV === 'production',
